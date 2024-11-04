0ef64bfc07cfcb832bcce575d781aef0d81304c4
ayankz <ayanpost87@gmail.com>
2024-11-05 00:15:29 +0500

    add styles, assets, eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavLinkComponent } from '../shared/components/nav-link/nav-link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavLinkComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'inveslink-app-test';
