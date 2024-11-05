b9f9785d8bd82072d5a34eed2b412e0bee5f16fd
ayankz <ayanpost87@gmail.com>
2024-11-06 00:45:45 +0500

    fix navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavLinkComponent } from '../shared/components/nav-link/nav-link.component';
+import { NavbarModule } from '../modules/navbar/navbar.module';
+import { CreateTaskModule } from '../modules/create-task/create-task.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavLinkComponent],
+  imports: [RouterOutlet, NavbarModule, CreateTaskModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/modules/navbar/types/task.ts
@@ -0,0 +1,5 @@
+export interface Task {
+  label: string;
+  icon: string;
+  status: string;
+}
