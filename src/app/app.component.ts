f8397032b6b0c13abbc396f5b413b41654875909
ayankz <ayanpost87@gmail.com>
2024-11-06 01:06:58 +0500

    add create page open/close to store service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarModule } from '../modules/navbar/navbar.module';
 import { CreateTaskModule } from '../modules/create-task/create-task.module';
+import { StoreService } from '../services/store.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarModule, CreateTaskModule],
+  imports: [RouterOutlet, NavbarModule, CreateTaskModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  public isVisibleCreatePage: Observable<boolean>;
+  constructor(private service: StoreService) {
+    this.isVisibleCreatePage = this.service.isVisibleCreateComponent$;
+  }
   title = 'inveslink-app-test';
 }
